0f24a696d0d0c1468ff80f9b00949f88cd7b2e65
Sreerag <sreeragmanoj07@gmail.com>
2025-09-14 12:50:18 +0530

    Ep 4 is completed and checked about routes

--- src/app.js
+++ src/app.js
@@ -2,14 +2,24 @@ const express = require('express')
 
 const app = express();
 
-// app.use('/', (req, res) => {
-//     res.send('this is the data from the main page')
-// })
 
 app.use('/test', (req, res) => {
     res.send('this is from the test page 34534534653456')
 })
 
+app.get('/getUser/:UserId/:password', (req, res) => {
+    console.log(req.params)
+    res.send(req.params)
+})
+
+app.get('/user', (req, res) => {
+    console.log(req.query)
+    res.send(req.query)
+})
+
+app.use('/', (req, res) => {
+    res.send('this is the data from the main page')
+})
 app.listen(3333, () => {
     console.log('the server is up and running fine')
 })
